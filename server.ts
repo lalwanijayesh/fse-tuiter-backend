f83784abf4ae753466bc52f53493599bee32f7da
Jayesh Lalwani <lalwanijayesh09@gmail.com>
2022-12-11 17:15:26 -0500

    Modify build process

--- server.ts
+++ server.ts
@@ -68,9 +68,9 @@ const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = "tuiter";
-// mongoose.connect(`${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, options);
-//mongoose.connect('mongodb://localhost:27017/tuiter', options);
-mongoose.connect('mongodb+srv://fse-team13:MVprxpWksVkla6vO@fse.yx5wwsz.mongodb.net/tuiter');
+mongoose.connect(`${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, options);
+// mongoose.connect('mongodb://localhost:27017/tuiter', options);
+// mongoose.connect('mongodb+srv://fse-team13:MVprxpWksVkla6vO@fse.yx5wwsz.mongodb.net/tuiter');
 
 // Inject RESTful controllers for web service API
 const userController = UserController.getInstance(app);
