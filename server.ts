06160322f36db91853724ce7b7ee62e7095b1fce
Jayesh Lalwani <lalwanijayesh09@gmail.com>
2022-12-12 14:18:57 -0500

    Update session properties specific to prod

--- server.ts
+++ server.ts
@@ -32,17 +32,17 @@ import StarController from "./controllers/StarController";
 const app = express();
 
 let sess = {
-    secret: "mysecret",
+    secret: process.env.SECRET,
     saveUninitialized: false,
     resave: false,
     cookie: {
-        secure: false
+        sameSite: process.env.NODE_ENV === "PRODUCTION" ? 'none' : 'lax',
+        secure: process.env.NODE_ENV === "PRODUCTION",
     }
 }
 
 if (process.env.ENV === 'PRODUCTION') {
     app.set('trust proxy', 1);
-    sess.cookie.secure = true;
 }
 
 app.use(session(sess));
